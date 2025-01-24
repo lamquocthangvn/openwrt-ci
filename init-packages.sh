a19ef1b7eee24bcf6eb1ad7d74ed7514355c6a68
midmanb <hatin068@gmail.com>
2025-01-24 11:58:07 +0700

    Fix

---
+++ init-packages.sh
@@ -0,0 +1,2 @@
+opkg update && opkg install luci-app-openvpn-server luci-app-ddns luci-app-ddns-go ddns-scripts-noip luci-app-cloudflared tailscale \
+  google-authenticator-libpam openssh-server-pam vim-fuller htop btop
